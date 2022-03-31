a0cff24bf77897eb56232fefd7a5920adef075bf
miha6g <72049060+miha6g@users.noreply.github.com>
2022-03-31 12:58:26 +0300

    Add files via upload

---
+++ zxc2.py
@@ -0,0 +1,32 @@
+import math
+import random
+while True:
+    a=input("Какую операцию вы хотите выполнить: +, - , / , * , exponentiation, module, random , factorial , arccos , exit : ")
+    if a in ('+', '-', '*', '/'):
+        x = float(input("x = "))
+        y = float(input("y = "))
+        if a =="+":
+            print(x+y)
+        if a =="-":
+            print(x-y)
+        if a =="*":
+            print(x*y)
+        if a =="/":
+            if y == 0:
+                print("Делить на 0 нельзя")
+                continue
+            print(x/y)
+    if a in ("exponentiation", "module", "factorial", "arccos"):
+        x = float(input("x = "))
+        if a == "exponentiation":
+            print(math.exp(x))
+        if a == "module":
+            print(math.fabs(x))
+        if a == "factorial":
+            print(math.factorial(x))
+        if a == "arccos":
+            print(math.acos(x))
+    if a == "random":
+        print(random.randint(0, 100))
+    if a == "exit":
+        break
