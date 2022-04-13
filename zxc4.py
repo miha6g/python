166ee86abe91aa5bb72ec4b517ab8b423b9ac492
User <misha000754@gmail.com>
2022-04-13 14:12:14 +0300

    fix

--- zxc4.py
+++ zxc4.py
@@ -1,9 +1,15 @@
 def create_set(title):
-    a=input(title).split(",")
+    a = input(title).split(",")
     print(a)
     return a
-first_set=create_set("Введите через запятую список слов: ")
-c=len(first_set)
-second_set=create_set("Первый список состоит из " +str(c) + " слов, введите еще один список с таким же количеством слов ")
-z = dict(zip(first_set, second_set))
-print(z)
+
+
+first_set = create_set("Введите через запятую список слов: ")
+c = len(first_set)
+second_set = create_set(
+    "Первый список состоит из " + str(c) + " слов, введите еще один список с таким же количеством слов")
+dictionary = {}
+
+for i in range(0, len(first_set)):
+    dictionary[first_set[i]] = second_set[i]
+print(dictionary)
