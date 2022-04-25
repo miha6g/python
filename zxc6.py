41ea00f09b2d083cb1fb11a482223d82ecacb4f5
User <misha000754@gmail.com>
2022-04-25 14:47:43 +0300

    fix

--- zxc6.py
+++ zxc6.py
@@ -1,39 +1,6 @@
-import math
-import random
+from mymodule import Calculator
 
 
-class Calculator:
-    @staticmethod
-    def basemath(operation):
-        x = float(input("x = "))
-        y = float(input("y = "))
-        if operation == "+":
-            print(x + y)
-        if operation == "-":
-            print(x - y)
-        if operation == "*":
-            print(x * y)
-        if operation == "/":
-            if y == 0:
-                print("Делить на 0 нельзя")
-                return
-            print(x / y)
-
-    @staticmethod
-    def mathpro(operation):
-        x = float(input("x = "))
-        if operation == "exponentiation":
-            print(math.exp(x))
-        if operation == "module":
-            print(math.fabs(x))
-        if operation == "factorial":
-            print(math.factorial(x))
-        if operation == "arccos":
-            print(math.acos(x))
-
-    @staticmethod
-    def rand():
-        print(random.randint(0, 100))
 
 
 while True:
